9d5080ff4dabb5dae1f7cc1402258627ecce46d2
mayara <mayakarolyne2015@gmail.com>
2019-08-26 11:48:11 -0300

    criando login e cadasreo

--- pi19oficina/urls.py
+++ pi19oficina/urls.py
@@ -15,10 +15,12 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path 
-from core.views import home
+from core.views import home, login, cadastro
 
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', home, name="home"),
+    path('login/', login, name="login"),
+    path('cadastro/', login, name="cadastro"),
 ]
