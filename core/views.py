a5178a7bf621719d85200eb45e7a46484f847cfb
greyce <greycekellynascimento9@gmail.com>
2019-09-02 13:00:13 -0300

    cadastro

--- core/views.py
+++ core/views.py
@@ -1,10 +1,21 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.contrib.auth.decorators import login_required
+from django.contrib.auth.forms import UserCreationForm
 
 # Create your views here.
 
 def home(request):
 	return render(request, 'home.html')
+@login_required
 def perfil(request):
 	return render(request, 'perfil.html')
 def cadastro(request):
-	return render(request, 'cadastro.html')
+	form = UserCreationForm(request.POST or None)
+	if form.is_valid():
+		form.save()
+		return redirect('login')
+	contexto = {
+	'form': form
+	}
+	return render(request, 'cadastro.html', contexto)
+	
