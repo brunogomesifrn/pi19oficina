0ca4b147497c28366eb0b1cd15728819fa5cfff3
greyce <greycekellynascimento9@gmail.com>
2019-09-02 12:51:12 -0300

    autenticacao

--- core/views.py
+++ core/views.py
@@ -4,7 +4,7 @@ from django.shortcuts import render
 
 def home(request):
 	return render(request, 'home.html')
-def login(request):
-	return render(request, 'login.html')
+def perfil(request):
+	return render(request, 'perfil.html')
 def cadastro(request):
 	return render(request, 'cadastro.html')
--- pi19oficina/urls.py
+++ pi19oficina/urls.py
@@ -15,12 +15,15 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path 
-from core.views import home, login, cadastro
+from core.views import home, cadastro, perfil
+from django.contrib.auth import views as auth_views
 
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', home, name="home"),
-    path('login/', login, name="login"),
+    path('login/', auth_views.LoginView.as_view(), name="login"),
     path('cadastro/', cadastro, name="cadastro"),
+    path('perfil/', perfil, name='perfil'),
+    path("logout/", auth_views.LogoutView.as_view(), name="logout"),
 ]
